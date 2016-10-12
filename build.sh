e09dcfceb9212ea36bd0a37b3f2510debfe07c54
skyxie <skyxie@gmail.com>
2016-10-12 16:07:56 -0400

    Fix deploy script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 rm sns_kinesis.zip
 zip -r sns_kinesis.zip index.js package.json node_modules --exclude=node_modules/aws-sdk/*
-aws lambda update-function-code --function-name test --zip-file fileb://sns_kinesis.zip
+aws lambda update-function-code --function-name sns_test --zip-file fileb://sns_kinesis.zip
